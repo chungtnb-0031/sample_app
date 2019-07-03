659717a3e14a84009f626a2dfb1b27e1107a2d0e
chungtnb-0031 <tran.ngoc.bao.chung@sun-asterisk.com>
2019-07-03 14:13:23 +0700

    chapter6+7

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+  before_save :downcase_email
+
+  validates :name,
+    presence: true,
+    length: {maximum: Settings.user_check.max_name}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :email,
+    presence: true,
+    length: {maximum: Settings.user_check.max_email},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+
+  has_secure_password
+  validates :password,
+    presence: true,
+    length: {minimum: Settings.user_check.min_password}
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+end
