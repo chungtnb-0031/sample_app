0f97d126cf36e6a3c91bf96e346b246b45324c05
chungtnb-0031 <tran.ngoc.bao.chung@sun-asterisk.com>
2019-07-05 15:23:37 +0700

    chapter9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new user_params
-    
     if @user.save
       flash[:success] = t ".notice"
       redirect_to @user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save :downcase_email
+  attr_accessor :remember_token
 
   validates :name,
     presence: true,
@@ -18,6 +19,35 @@ class User < ApplicationRecord
     presence: true,
     length: {minimum: Settings.user_check.min_password}
 
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password? (remember_token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
   private
 
   def downcase_email
@@ -31,7 +61,7 @@ class User < ApplicationRecord
              else
                BCrypt::Engine.cost
              end
-      BCrypt::Password.create(string, cost: cost)
+      BCrypt::Password.create string, cost: cost
     end
   end
 end
