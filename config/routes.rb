8e57e8bc5485822e74a56e541dcaaed4c2e592fc
chungtnb-0031 <tran.ngoc.bao.chung@sun-asterisk.com>
2019-07-16 11:11:46 +0700

    chapter14

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,14 @@ Rails.application.routes.draw do
     get "(page/:page)", action: :index, on: :collection, as: ""
   end
 
-  resources :users, concerns: :paginatable
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :account_activations, only: :edit
   resources :password_resets, except: %i(index show destroy)
   resources :microposts, only: %i(create destroy)
+  resources :relationships, only: %i(create destroy)
 end
